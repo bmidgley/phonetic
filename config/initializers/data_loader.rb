23dc1d7a1cf15f21d6d7045d4ef77ad04cd26b92
Brad Midgley <bmidgley@rbcon.com>
2012-09-28 19:02:58 -0600

    background db init

--- config/initializers/data_loader.rb
+++ config/initializers/data_loader.rb
@@ -1,5 +1,9 @@
-if Conversion.count.zero?
-  load "#{Rails.root}/Rakefile"
-  Rake::Task['db:seed'].invoke
+begin
+  if Conversion.count.zero?
+    #load "#{Rails.root}/Rakefile"
+    #Rake::Task['db:seed'].invoke
+    system("rake db:seed >/dev/null 2>&1 &")
+  end
+rescue
+  puts "skipping auto seed"
 end
-
